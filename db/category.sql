5cffe7a982c9377ce3c379d83be5f613c65c8574
Yuya Nishiyama <nsyee@popstack.net>
2012-10-12 15:22:53 +0900

    テスト用のカテゴリを作成。

---
+++ db/category.sql
@@ -0,0 +1,5 @@
+INSERT INTO category(title, count, rank, created_at) VALUES('グルメ', 0, 1, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('旅行', 0, 2, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('映画', 0, 3, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('音楽', 0, 4, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('生活', 0, 5, NOW());

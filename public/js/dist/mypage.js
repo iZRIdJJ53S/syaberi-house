b8d7c99e723e810bbbf3131942599d68b1edf400
Yuya Nishiyama <nsyee@popstack.net>
2012-11-06 15:19:05 +0900

    チャットルームの文字数制限を３〜２０から、５〜１００に変更

--- public/js/dist/mypage.js
+++ public/js/dist/mypage.js
@@ -122,11 +122,11 @@
       ],
       title: [
         { required: true, msg: 'チャットルーム名を入力してください' },
-        { rangeLength: [3, 20], msg: 'チャットルーム名は3文字以上20文字以下で入力してください' }
+        { rangeLength: [5, 100], msg: 'チャットルーム名は5文字以上100文字以下で入力してください' }
       ],
       description: [
         { required: true, msg: '説明を入力してください' },
-        { rangeLength: [3, 200], msg: 'チャットルーム名は3文字以上200文字以下で入力してください' }
+        { rangeLength: [3, 10000], msg: '説明は3文字以上10000文字以下で入力してください' }
       ],
       publicYN: [
         { required: true, msg: '公開設定を選択してください' }

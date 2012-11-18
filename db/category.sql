4a7a33e09aa9c80ac4320f165f20d2bf59f9f8ee
y-saito <majesta0110@gmail.com>
2012-11-18 13:28:11 +0900

    res.redirect の不具合修正と、カテゴリーの修正

--- db/category.sql
+++ db/category.sql
@@ -1,5 +1,7 @@
-INSERT INTO category(title, count, rank, created_at) VALUES('グルメ', 0, 1, NOW());
-INSERT INTO category(title, count, rank, created_at) VALUES('旅行', 0, 2, NOW());
-INSERT INTO category(title, count, rank, created_at) VALUES('映画', 0, 3, NOW());
-INSERT INTO category(title, count, rank, created_at) VALUES('音楽', 0, 4, NOW());
-INSERT INTO category(title, count, rank, created_at) VALUES('生活', 0, 5, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('生活', 0, 1, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('仕事', 0, 2, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('趣味', 0, 3, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('エンタメ', 0, 4, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('グルメ', 0, 5, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('雑談', 0, 6, NOW());
+INSERT INTO category(title, count, rank, created_at) VALUES('ひみつ', 0, 7, NOW());
